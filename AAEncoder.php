4625db2f4835687126d5ff17cdb3d37cbb41fb2d
Andrey Izman <izman.dev@gmail.com>
2015-11-21 17:11:31 +0200

    Added encoding level

--- AAEncoder.php
+++ AAEncoder.php
@@ -14,30 +14,12 @@ class AAEncoder
     /**
      * Encode any JavaScript program to Japanese style emoticons (^_^)
      * @param string $js
+     * @param int $level [optional]
      * @return string
      */
-    public static function encode($js)
+    public static function encode($js, $level=0)
     {
-        $b = [
-            "(c^_^o)" ,
-            "(ﾟΘﾟ)" ,
-            "((o^_^o) - (ﾟΘﾟ))" ,
-            "(o^_^o)" ,
-            "(ﾟｰﾟ)" ,
-            "((ﾟｰﾟ) + (ﾟΘﾟ))" ,
-            "((o^_^o) +(o^_^o))" ,
-            "((ﾟｰﾟ) + (o^_^o))" ,
-            "((ﾟｰﾟ) + (ﾟｰﾟ))" ,
-            "((ﾟｰﾟ) + (ﾟｰﾟ) + (ﾟΘﾟ))" ,
-            "(ﾟДﾟ) .ﾟωﾟﾉ" ,
-            "(ﾟДﾟ) .ﾟΘﾟﾉ" ,
-            "(ﾟДﾟ) ['c']" ,
-            "(ﾟДﾟ) .ﾟｰﾟﾉ" ,
-            "(ﾟДﾟ) .ﾟДﾟﾉ" ,
-            "(ﾟДﾟ) [ﾟΘﾟ]"
-        ];
-
-        $r = "ﾟωﾟﾉ= /｀ｍ´）ﾉ ~┻━┻   //*´∇｀*/ ['_']; o=(ﾟｰﾟ)  =_=3; c=(ﾟΘﾟ) =(ﾟｰﾟ)-(ﾟｰﾟ); " .
+        $result = "ﾟωﾟﾉ= /｀ｍ´）ﾉ ~┻━┻   //*´∇｀*/ ['_']; o=(ﾟｰﾟ)  =_=3; c=(ﾟΘﾟ) =(ﾟｰﾟ)-(ﾟｰﾟ); " .
             "(ﾟДﾟ) =(ﾟΘﾟ)= (o^_^o)/ (o^_^o);" .
             "(ﾟДﾟ)={ﾟΘﾟ: '_' ,ﾟωﾟﾉ : ((ﾟωﾟﾉ==3) +'_') [ﾟΘﾟ] " .
             ",ﾟｰﾟﾉ :(ﾟωﾟﾉ+ '_')[o^_^o -(ﾟΘﾟ)] " .
@@ -57,27 +39,82 @@ class AAEncoder
             "(oﾟｰﾟo)=(ﾟωﾟﾉ +'_')[c^_^o];" .
             "(ﾟДﾟ) [ﾟoﾟ]='\\\"';" .
             "(ﾟДﾟ) ['_'] ( (ﾟДﾟ) ['_'] (ﾟεﾟ+" .
-            "(ﾟДﾟ)[ﾟoﾟ]+ ";
+            "/*´∇｀*/(ﾟДﾟ)[ﾟoﾟ]+ ";
 
         for ($i = 0, $len = mb_strlen($js); $i < $len; $i++) {
-            $n = unpack('N', mb_convert_encoding(mb_substr($js, $i, 1, 'UTF-8'), 'UCS-4BE', 'UTF-8'))[1];
-            $t = "(ﾟДﾟ)[ﾟεﾟ]+";
-            if ($n <= 127) {
-                $t .= preg_replace_callback('/([0-7])/', function($m) use ($b) {
-                    return $b[intval($m[1])] . '+ ';
-                }, decoct($n));
+            $code = unpack('N', mb_convert_encoding(mb_substr($js, $i, 1, 'UTF-8'), 'UCS-4BE', 'UTF-8'))[1];
+            $text = '(ﾟДﾟ)[ﾟεﾟ]+';
+            if ($code <= 127) {
+                $text .= preg_replace_callback('/([0-7])/', function($match) use ($level) {
+                    $byte = intval($match[1]);
+                    return ($level ? self::randomize($byte, $level) : self::$bytes[$byte]) . '+';
+                }, decoct($code));
             }
             else {
-                $m = str_split(strtolower(substr('000' . dechex($n), -4)));
-                $t .= "(oﾟｰﾟo)+ ";
+                $match = str_split(substr('000' . dechex($code), -4));
+                $text .= "(oﾟｰﾟo)+ ";
                 do {
-                    $t .= $b[hexdec(array_shift($m))] . '+ ';
-                } while (count($m));
+                    $byte = hexdec(array_shift($match));
+                    $text .= self::$bytes[$byte] . '+ ';
+                } while (count($match));
             }
-            $r .=  $t;
+            $result .=  $text;
 
         }
-        $r .= "(ﾟДﾟ)[ﾟoﾟ]) (ﾟΘﾟ)) ('_');";
-        return $r;
+        $result .= "(ﾟДﾟ)[ﾟoﾟ]) (ﾟΘﾟ)) ('_');";
+        return $result;
+    }
+
+    /**
+     * @var array
+     */
+    protected static $bytes = [
+        "(c^_^o)",
+        "(ﾟΘﾟ)",
+        "((o^_^o) - (ﾟΘﾟ))",
+        "(o^_^o)",
+        "(ﾟｰﾟ)",
+        "((ﾟｰﾟ) + (ﾟΘﾟ))",
+        "((o^_^o) +(o^_^o))",
+        "((ﾟｰﾟ) + (o^_^o))",
+        "((ﾟｰﾟ) + (ﾟｰﾟ))",
+        "((ﾟｰﾟ) + (ﾟｰﾟ) + (ﾟΘﾟ))",
+        "(ﾟДﾟ) .ﾟωﾟﾉ",
+        "(ﾟДﾟ) .ﾟΘﾟﾉ",
+        "(ﾟДﾟ) ['c']",
+        "(ﾟДﾟ) .ﾟｰﾟﾉ",
+        "(ﾟДﾟ) .ﾟДﾟﾉ",
+        "(ﾟДﾟ) [ﾟΘﾟ]",
+    ];
+
+    /**
+     * @param int $byte
+     * @param int $level
+     * @return string
+     */
+    protected static function randomize($byte, $level)
+    {
+        $random = [
+            0 => [['+0', '-0'], ['+1', '-1'], ['+3', '-3'], ['+4', '-4']],
+            1 => [['+1', '-0'], ['+3', '-1', '-1'], ['+4', '-3']],
+            2 => [['+3', '-1'], ['+4', '-3', '+1'], ['+3', '+3', '-4']],
+            3 => [['+3', '-0'], ['+4', '-3', '+1', '+1']],
+            4 => [['+4', '+0'], ['+1', '+3'], ['+4', '-0']],
+            5 => [['+3', '+1', '+1'], ['+4', '+1'], ['+3', '+3', '-1']],
+            6 => [['+3', '+3'], ['+4', '+1', '+1'], ['+4', '+3', '-1']],
+            7 => [['+3', '+4'], ['+3', '+3', '+1'], ['+4', '+4', '-1']],
+        ];
+        if ($byte <= 8) {
+            while ($level--) {
+                $byte = preg_replace_callback('/[0-7]/', function($match) use ($random) {
+                    $numbers = $random[$match[0]][mt_rand(0, count($random[$match[0]]) - 1)];
+                    shuffle($numbers);
+                    $byte = ltrim(implode('', $numbers), '+');
+                    return strlen($byte) > 2 ? "($byte)" : $byte;
+                }, $byte);
+            }
+            $byte = str_replace('+-', '-', $byte);
+        }
+        return str_replace(array_keys(self::$bytes), self::$bytes, $byte);
     }
 }
